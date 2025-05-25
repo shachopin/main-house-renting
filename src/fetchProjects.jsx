7fc554df7bd06c02bd0fdc05b8cd4a29d1c2bf5b
shachopin <shachopin@gmail.com>
2025-05-25 15:55:25 +0000

    first

---
+++ src/fetchProjects.jsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react';
+import { createClient } from 'contentful';
+
+const client = createClient({
+  // space: '3e7vobor0uwr',
+  // environment: 'master',
+  // accessToken: import.meta.env.VITE_API_KEY,
+  //below working 
+  // space: "qz00uzgg3leh",
+  // environment: "master",
+  // accessToken: "9F-ayYfAgGWKUCgKbyTgjgS2CiBLM141-ScE_5ewlv0"
+  // my own below
+  space: 'vvnirsdlkyhy',
+  environment: 'master', // defaults to 'master' if not set
+  accessToken: '8aT1iDnqjO5Xy4htPvFJ9bZlzyMIGQQ9drvIa_uR_Ag'
+});
+
+export const useFetchProjects = () => {
+  const [loading, setLoading] = useState(true);
+  const [projects, setProjects] = useState([]);
+
+  const getData = async () => {
+    try {
+      const response = await client.getEntries({ content_type: 'projects' });
+      const projects = response.items.map((item) => {
+        const { title, url, image } = item.fields;
+        const id = item.sys.id;
+        const img = image?.fields?.file?.url;
+        return { title, url, id, img };
+      });
+      setProjects(projects);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+  return { loading, projects };
+};
